ab1af5435dee6465bc3f8e387e64b364e3148604
P K Nayak <47595700+MekongDelta-mind@users.noreply.github.com>
2024-09-19 10:51:17 +0530

    saving json into data folder;

--- data/make_dataset.py
+++ data/make_dataset.py
@@ -1,3 +1,3 @@
 '''
-THis would serve as a file to bring the data from the website and put it into the data/raw
+THis would serve as a file to bring the data from the website and put it into the data/raw in the json format
 '''
